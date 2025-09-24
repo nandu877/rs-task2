e84755339c8f02afa5e3e9806cd16f2c82157638
nandu877 <nandiniuppala877@gmail.com>
2025-09-24 23:34:07 +0530

    palindrome

--- Palindome.java
+++ Palindome.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 class Palindrome{
     public static void main(String[] args) {
-        System.out.println("djkfhkjsdfjk");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String str = sc.nextLine();
+        String revString = "";
+        for(int i = 0;i<str.length();i++)
+        {
+            revString =   str.charAt(i) + revString ;
+        }
+        if(str.equals(revString))
+            System.out.println("Given string is a palindrome");
+        else
+            System.out.println("Given string is not a palindrome");
+
     }
 }
