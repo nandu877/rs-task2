d800ac52e36849196a5cfc10ab817d9281e94257
nandu877 <nandiniuppala877@gmail.com>
2025-09-24 14:11:40 +0530

    prime

--- Prime.java
+++ Prime.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Prime{
     public static void main(String[] args) {
-        System.out.println("HElloo Jstar");
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n = sc.nextInt();
+
+        boolean flag = true;
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+            System.out.println("Given number "+n+" is a prime number");
+        else
+            System.out.println("Given number "+n+" is not a prime number");
     }
 }
